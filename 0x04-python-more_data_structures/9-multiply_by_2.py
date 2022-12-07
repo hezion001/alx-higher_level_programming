74d92b6511b9d5b45796d1e7a5c8dde649dbc69e
hezion001 <ibok.hezekiah@gmail.com>
2022-12-08 00:59:22 +0100

    Task 9-multiply completed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    new_dict = {}
-    if my_dict:
-        for key, val in my_dict.items():
-            new_dict.update({key: val * 2})
-        return (new_dict)
+def multiply_by_2(a_dictionary):
+    return {k: v * 2 for k, v in a_dictionary.items()}
