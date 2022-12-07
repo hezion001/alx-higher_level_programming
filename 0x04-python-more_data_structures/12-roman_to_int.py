7adcdb2676d164e6ed04bba49034fe22ea04522b
hezion001 <ibok.hezekiah@gmail.com>
2022-12-08 00:52:18 +0100

    Task modified

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or not roman_string:
-        return 0
-    roman_string = roman_string.lower()
-    tbl = {'': 0, 'i': 1, 'v': 5, 'x': 10,
-           'l': 50, 'c': 100, 'd': 500, 'm': 1000}
-    i = 0
-    num = 0
-    l = len(roman_string)
-    while i < l:
-        c0 = roman_string[i]
-        c1 = roman_string[i + 1] if l - 1 > i else ''
-        v0 = tbl[c0]
-        v1 = tbl[c1]
-        if v1 > v0:
-            num += v1 - v0
-            i += 1
-        else:
-            num += v0
-        i += 1
-    return num
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    if (type(roman_string) is not str or roman_string is None):
+        return (0)
+    for i, char in enumerate(roman_string):
+        tmp_num = roman[char]
+        try:
+            if tmp_num < roman[roman_string[i + 1]]:
+                tmp_num = tmp_num * -1
+        except IndexError:
+            pass
+        number = number + tmp_num
+    return (number)
